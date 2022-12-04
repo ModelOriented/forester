89950ca051654b8c3a367fb2c08bb9224b4e4a2e
grudziena <adriannagrudzien314@gmail.com>
2022-12-04 14:41:26 +0100

    comparable data split

--- R/train_test_balance.R
+++ R/train_test_balance.R
@@ -8,6 +8,8 @@
 #' @param balance A logical value, determines if we want to balance the dataset.
 #' @param fractions A vector with 3 numeric values that sum to 1 which
 #' determine sizes of train, test and validation datasets. DEFAULT: c(0.6, 0.2, 0.2).
+#' @param seed An integer random seed. It allows for comparable results. If it
+#' is NULL, the split is random.
 #'
 #' @return A list of train, test and validation datasets.
 #' @export
@@ -17,17 +19,19 @@
 #' b_lisbon <- train_test_balance(lisbon, 'Price', balance = FALSE, 'regression',
 #'                                fractions = c(train = 0.6, valid = 0.2, test = 0.2))
 train_test_balance <-
-  function(data, y, type, balance = TRUE, fractions = c(0.6, 0.2, 0.2)) {
+  function(data, y, type, balance = TRUE, fractions = c(0.6, 0.2, 0.2), seed = NULL) {
     #Balancing / stratifying classes.
     target <- data[[y]]
     if (balance == TRUE) {
       inds  <- splitTools::partition(target, p = c(train = fractions[1],
                                                    test  = fractions[2],
-                                                   valid = fractions[3]))
+                                                   valid = fractions[3]),
+                                     seed = seed)
     } else {
       inds  <- splitTools::partition(target, p = c(train = fractions[1],
                                                    test  = fractions[2],
                                                    valid = fractions[3]),
+                                     seed = seed,
                                      type = 'basic')
     }
 
